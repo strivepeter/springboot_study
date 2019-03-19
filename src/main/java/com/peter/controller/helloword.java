c30032995d428a45881679c9062fdaec0a439bc1
peter <1095875051@qq.com>
2019-03-19 16:21:28 +0800

    新增了helloWord测试

---
+++ src/main/java/com/peter/controller/helloword.java
@@ -0,0 +1,16 @@
+package com.peter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2019/3/19.
+ */
+@RestController
+public class helloword {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
